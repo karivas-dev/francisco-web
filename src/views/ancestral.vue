1888a849e366ae7d639259b361351fb541b86d4c
Karen Rivas <karenrivasg17@gmail.com>
2024-01-06 20:24:21 -0600

    Finish Ancestral

--- src/views/ancestral.vue
+++ src/views/ancestral.vue
@@ -1,30 +1,66 @@
 <script setup>
-
+import OpeningTitle from '@/components/OpeningTitle.vue';
 </script>
 
 <template>
-  <img alt="Ancestral banner" class="w-full mb-24" src="../assets/img/ancestral/banner-ancestral.png"/>
+  <img alt="Banner Ancestral" class="w-full mb-20" src="../assets/img/ancestral/banner-ancestral.png" />
 
-  <div class="mx-10">
+  <OpeningTitle>
+    <img alt="Titulo" width="100%" src="@/assets/img/ancestral/title-ancestral.png" />
+  </OpeningTitle>
 
-    <div class="grid grid-cols-2">
-      <p class="my-auto">La Organización de las Naciones Unidas para la Alimentación y la Agricultura desarrolló un proyecto enfocado en
+  <div class="mx-32">
+    <div class="flex justify-center m-8">
+      <span class="px-5 py-4 border border-[#] rounded-full">Menú// Alcances: Ilustración, Diseño de personajes,
+        Editorial</span>
+    </div>
+
+    <div class="grid grid-cols-2 mx-32 gap-x-12">
+      <p class="my-auto">La Organización de las Naciones Unidas para la Alimentación y la Agricultura desarrolló un
+        proyecto enfocado en
         la promulgación de la herencia indígena y afrodescendiente de los platillos del territorio salvadoreño, a través
         de un Recetario ancestral, con alimentos nutritivos y de fácil preparación. Para este proyecto se trabajó de
         manera colaborativa, y tuve la oportunidad de ejecutar la dirección fotográfica, así como el food styling de
         dichos platos.</p>
-      <img alt="Timi banner" class="w-full mb-24" src="../assets/img/ancestral/foto06.png"/>
+      <img alt="Foto" class="w-full mb-16" src="../assets/img/ancestral/foto06.png" />
     </div>
+  </div>
 
-    <div class="grid grid-cols-3 gap-x-5">
-      <div class="col-span-1 grid-rows-2 ">
-        <img alt="Foto" class="w-full" src="../assets/img/ancestral/foto01.png"/>
-        <img alt="Foto" class="w-full" src="../assets/img/ancestral/foto02.png"/>
-      </div>
-
-      <div class="col-span-2">
-        <img alt="Foto" class="w-full mb-24" src="../assets/img/ancestral/foto03.png"/>
+  <div class="grid grid-cols-8 gap-2 mx-8">
+    <div class="col-span-3 ">
+      <div class="grid-rows-2 gap-y-2">
+        <img alt="Foto" class="w-full row-span-1" src="../assets/img/ancestral/foto01.png" />
+        <img alt="Foto" class="w-full row-span-1" src="../assets/img/ancestral/foto02.png" />
       </div>
     </div>
+
+    <div class="col-span-5">
+      <img alt="Foto" class="w-full mb-24" src="../assets/img/ancestral/foto03.png" />
+    </div>
+  </div>
+
+  <div class="grid grid-cols-8 mx-8 gap-x-2">
+    <img alt="Foto" width="100%" class="col-span-4 col-start-3" src="@/assets/img/ancestral/subtitulo.png" />
+    <p class="col-start-2 col-span-6 mt-8 mb-12">
+      El registro fotográfico nos llevó a realizar trabajo de campo como parte del proceso. Las recetas son resultado de la
+      investigación y registro desarrollado por el chef Cipactli para FAO El Salvador. Trabajar de manera multidisciplinaria
+      y colaborativa es un componente del diseño flexible como parte fundamental en este proyecto.
+    </p>
+
+    <img alt="Foto" width="100%" class="col-span-2 col-start-2" src="@/assets/img/ancestral/foto05.png" />
+    <img alt="Foto" width="100%" class="col-span-2" src="@/assets/img/ancestral/foto07.png" />
+    <img alt="Foto" width="100%" class="col-span-2" src="@/assets/img/ancestral/foto04.png" />
+  </div>
+  
+  <div class="grid grid-cols-8 mx-8 gap-x-2 mt-3">
+    <img alt="Foto" width="100%" class="col-span-5" src="@/assets/img/ancestral/foto08.png" />
+    <img alt="Foto" width="100%" class="col-span-3" src="@/assets/img/ancestral/foto09.png" />
+  </div>
+
+  <div class="my-12">
+    <p class="text-center font-bold">Créditos<span class="text-[#fbb91c]">//</span></p>
+    <p class="text-center">Fotografía y Post-producción_Otoniel López</p>
+    <p class="text-center">Dirección y Food-styling_Francisco Rivas</p>
+    <p class="text-center">Cocina y Coordinación_Chef Jorge Cipactli</p>
   </div>
 </template>
