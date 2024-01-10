fc0c92811e8c4112ebf522ea384c55b7988b0506
karivas-dev <karenrivasg17@gmail.com>
2024-01-09 22:41:55 -0600

    Add banner home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,13 +4,13 @@ import Button from "@/components/Button.vue";
 </script>
 
 <template>
-  <div class="h-[calc(100vh-70px)] bg-gray-500 relative">
-   <!-- <video autoplay loop muted width="100%">
-      <source src="/src/assets/Post-Mestizo-v1.mp4" type="video/mp4"/>
+  <div class="relative">
+    <video autoplay loop muted width="100%">
+      <source src="@/assets/videos/main-gradient.mp4" type="video/mp4"/>
       Your browser does not support the video tag
-    </video>-->
+    </video>
 
-    <div class="absolute bottom-0 -translate-x-1/2 left-1/2 w-5/6 lg:w-4/6 font-agrandir mb-20
+    <div class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 w-5/6 lg:w-4/6 font-agrandir
     font-extrabold font-extraexpanded text-center space-y-10 text-[#323231] z-10">
       <p class="text-2xl md:text-5xl">La inspiración está en la oportunidad de darnos variedad para crear y aprender</p>
       <Button>Mi trabajo</Button>
