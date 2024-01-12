1518c566971566ea06bc09160d57196295de94f0
karivas-dev <karenrivasg17@gmail.com>
2024-01-12 16:31:25 -0600

    Portafolio source update

--- src/views/portafolio.vue
+++ src/views/portafolio.vue
@@ -2,9 +2,8 @@
 </script>
 
 <template>
-  <div class="flex justify-center my-20">
-
-    <object data='/portafolio.pdf'    width="800"
+  <div class="flex justify-center my-20 mx-8">
+    <object data='https://drive.google.com/file/d/1Ct2E9J7yNGKs0V2nOPSq30PQkv7YL33p/preview?usp=sharing'    width="800"
             height="500">
       <p><a href="/portafolio.pdf">Descargar portafolio</a></p>
     </object>
